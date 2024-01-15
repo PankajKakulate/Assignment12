f43cf6880272951009fd94e29ec8008452224225
pankaj <pankaj7187815@gmail.com>
2024-01-15 22:29:42 +0530

    I C 15-01-2024 10:29 PM

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { BugInfoComponent } from './bug-info/bug-info.component';
+import { PersonalInfoComponent } from './personal-info/personal-info.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet,BugInfoComponent,PersonalInfoComponent,FooterComponent,HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
